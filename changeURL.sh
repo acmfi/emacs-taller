0ef6ccd1e7d39e7d601b88530b3297501a799b4e
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2016-03-19 03:03:18 +0100

    Esto pasa PORNO escribir bien los paths.

--- changeURL.sh
+++ changeURL.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sed -i -e 's"file:/home/coyote3007/.emacs.d/reveal.js-master"./reveal.js"g' *.html
-sed -i -e 's"file:///home/ignaciobll/reveal.js-master"./reveal.js"g' *.html
+sed -i -e 's"file:///home/ignaciobll/reveal.js"./reveal.js"g' *.html
